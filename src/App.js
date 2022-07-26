164368bcd83f62e91058cb1a58fe86710040c4d5
Vadivel <dbxdev.anandh@prema.consulting>
2022-07-26 12:14:33 +0100

    updated login func.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, { useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import './App.css';
 import Feeds from './Components/Feeds/Feeds';
 import Header from './Components/header/Header';
 import Sidebar from './Components/Sidebar/Sidebar';
-import { selectUser } from './features/userSlice';
+import { selectUser,login,logout } from './features/userSlice';
 import Login from '../src/Components/Login/Login'
+import { auth } from './firebase';
 
 function App() {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((userAuth) => {
+      if(userAuth){
+        // user is logged in
+        dispatch(
+          login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+      }else{
+        dispatch(logout());
+
+      }
+    })
+  })
   return (
     <div className="app">
       <Header/>
